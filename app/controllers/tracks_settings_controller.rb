8f95b616f28898a8f7dd19596245a6425c96ee14
Marek Hulan <ares@igloonet.cz>
2011-04-07 15:55:56 +0200

    Fix for date that are in past

--- app/controllers/tracks_settings_controller.rb
+++ app/controllers/tracks_settings_controller.rb
@@ -31,9 +31,9 @@ class TracksSettingsController < ApplicationController
     notes = url_for(:controller => 'issues', :action => 'show', :id => @issue, :only_path => false) + "\n\n" + notes
     
     todo = Todo.new(:context_id => params[:context_id], :project_id => params[:project_id],
-          :description => @issue.to_s, :notes => notes, 
-          :due => @issue.due_date.try(:strftime, "%d/%m/%Y"),
-          :show_from => @issue.start_date.try(:strftime, "%d/%m/%Y"))
+          :description => @issue.to_s, :notes => notes)
+    todo.show_from = @issue.start_date.strftime("%d/%m/%Y") if @issue.start_date.present? && @issue.start_date >= 1.day.ago.to_date
+    todo.due = @issue.due_date.strftime("%d/%m/%Y") if @issue.due_date.present? && @issue.due_date >= 1.day.ago.to_date
     
     if @result = todo.save
       @result &= @issue.update_attributes :tracks_url => todo.full_url, :tracks_todo_id => todo.id
