4d6cd52295de6d280bc99d07ab365a1af831cf9a
Marek Hulan <ares@igloonet.cz>
2011-07-04 09:11:19 +0200

    Added possibility to synchronize dates
    see new options on tracks setting page

--- lib/tracks_issue_hook.rb
+++ lib/tracks_issue_hook.rb
@@ -5,21 +5,18 @@ class TracksIssueHook  < Redmine::Hook::ViewListener
   # :params, :issue
   def controller_issues_edit_before_save(context = {})
     tracks_link = TracksLink.find_by_issue_id_and_user_id(context[:issue].id, User.current.id)
-    if User.current.tracks_url.present? && 
-      tracks_link.present? && 
-      context[:issue].closing?
-      toggle_todo(context, tracks_link, 'active')
+    if User.current.tracks_url.present? && tracks_link.present?
+      toggle_todo(context, tracks_link, 'active') if context[:issue].closing?
+      update_todo(tracks_link) if User.current.tracks_time_sync
     end 
   end
   
   # :params, :issue, :time_entry, :journal
   def controller_issues_edit_after_save(context = {})
     tracks_link = TracksLink.find_by_issue_id_and_user_id(context[:issue].id, User.current.id)
-    if User.current.tracks_url.present? && 
-      tracks_link.present? && 
-      context[:issue].changed? && 
-      context[:issue].closing?
-      toggle_todo(context, tracks_link, 'completed')
+    if User.current.tracks_url.present? && tracks_link.present? && context[:issue].changed?
+      toggle_todo(context, tracks_link, 'completed') if context[:issue].closing?
+      update_todo(tracks_link) if User.current.tracks_time_sync
     end
   end
   
@@ -42,6 +39,23 @@ class TracksIssueHook  < Redmine::Hook::ViewListener
     begin
       todo = Todo.find(tracks_link.tracks_todo_id)
       todo.put(:toggle_check) if todo.state == state
+    rescue Exception => e
+      # silently ignore all messages and log them
+      Rails.logger.error "Tracks connection error"
+      Rails.logger.error e.message
+      Rails.logger.error e.backtrace.join("\n")
+    end
+  end
+  
+  def update_todo(tracks_link)
+    setup_todo
+    begin
+      todo = Todo.find(tracks_link.tracks_todo_id)
+      todo.show_from = tracks_link.issue.start_date.strftime(User.current.tracks_time_format) if tracks_link.issue.start_date.present? && tracks_link.issue.start_date >= Date.today
+      todo.due = tracks_link.issue.due_date.strftime(User.current.tracks_time_format) if tracks_link.issue.due_date.present? && tracks_link.issue.due_date >= Date.today
+
+      todo.save
+
     rescue Exception => e
       # silently ignore all messages and log them
       Rails.logger.error "Tracks connection error"
